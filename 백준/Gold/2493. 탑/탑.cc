2831d0430ca7480c3bebbe70f6de8e6c81d9d181
dameyaa <59946480+jung-dam@users.noreply.github.com>
2024-04-06 13:16:38 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+ 
+using namespace std;
+ 
+int main() 
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+ 
+    stack<pair<int, int> > s;
+    int num, height;
+    cin >> num;
+ 
+    for (int i = 0; i < num; i++) 
+    {
+        cin >> height;
+ 
+        while (!s.empty()) 
+        {
+            //수신탑 발견했을 때
+            if (height < s.top().second) 
+            { 
+                cout << s.top().first << " ";
+                break;
+            }
+            s.pop();
+        }
+        //수신 탑이 없다면
+        if (s.empty()) 
+        {
+            cout << 0 << " ";
+        }
+        s.push(make_pair(i + 1, height));
+    }
+     
+    return 0;
+} 
